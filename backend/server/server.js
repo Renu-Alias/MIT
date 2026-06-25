1e51c4fed31a1926d4c5ea8c47f8b4a623b482a8
Renu Alias <renualiasmeleth@gmail.com>
2026-06-25 11:34:55 +0530

    Create server.js

---
+++ backend/server/server.js
@@ -0,0 +1,26 @@
+const express=require('express');
+const app=express();
+
+const PORT=5001; //Port change
+app.use(express.json());
+
+const books=[{id:1,name:"Book 1",author:"Author 1"},
+            {id:2,name:"Book 2",author:"Author 2"},
+            {id:3,name:"Book 3",author:"Author 3"},
+            {id:4,name:"Book 4",author:"Author 4"},
+            {id:5,name:"Book 5",author:"Author 5"}    
+];
+
+app.get("/getbooks",(req,res) =>{
+    res.send(books);
+})
+
+app.get("/getdata",(req,res)=>{
+    res.send("Hello from the server");
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
+
+
